cefe8236aac0c3235a13a8c2077e0e600b8893ca
Michał Bartkowiak <bartmichu@gmail.com>
2022-05-04 18:21:52 +0200

    Rename script

--- scripts/lynis-warnings-email.sh
+++ scripts/lynis-warnings-email.sh
@@ -5,7 +5,6 @@ email_subject="`/bin/hostname` - Lynis Audit Warnings"
 
 lynis_report="`/usr/sbin/lynis audit system --cronjob --warnings-only`"
 
-if [ -n "$lynis_report" ]
-then
+if [ -n "$lynis_report" ]; then
    /usr/bin/printf '%s\n' "$email_subject" "" "$lynis_report" | /usr/bin/mail -s "$email_subject" "$email_address"
 fi
